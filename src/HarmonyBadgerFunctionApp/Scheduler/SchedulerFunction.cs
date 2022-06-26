f3e4929d3b9a7406d74833916ce8f91c2ad1d3d0
Alexandre Kerametlian <akerametlian@outlook.com>
2022-06-25 21:43:20 -0700

    Implement experimental code in scheduler function

--- src/HarmonyBadgerFunctionApp/Scheduler/SchedulerFunction.cs
+++ src/HarmonyBadgerFunctionApp/Scheduler/SchedulerFunction.cs
@@ -1,7 +1,11 @@
 using System;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using NCrontab;
+using TimeZoneConverter;
 
 namespace HarmonyBadgerFunctionApp.Scheduler;
 
@@ -9,14 +13,37 @@ public class SchedulerFunction
 {
     private const string HourlyTrigger = "0 0 */1 * * *";
 
+    private const string Every30SecondsTrigger = "*/30 * * * * *";
+
     private const string Every10SecondsTrigger = "*/10 * * * * *";
 
     [FunctionName("HarmonyBadger_Scheduler")]
-    public void Run(
+    public async Task RunAsync(
         [TimerTrigger(Every10SecondsTrigger)] TimerInfo myTimer,
-        ILogger log)
+        ILogger log,
+        ExecutionContext context)
     {
-        var files = string.Join(", ", Directory.EnumerateFiles(Constants.TaskConfigsDirectoryName));
-        log.LogInformation($"[{DateTime.Now}] Timer triggered, found config files: {files}");
+        await Task.Yield();
+
+        var tzInfo = TZConvert.GetTimeZoneInfo("US/Pacific");
+        var localTime = TimeZoneInfo.ConvertTime(DateTime.UtcNow, tzInfo);
+
+        var crontabSchedule = CrontabSchedule.Parse(
+            Every30SecondsTrigger,
+            new CrontabSchedule.ParseOptions
+            {
+                IncludingSeconds = true,
+            });
+        var numOccurrences = crontabSchedule
+            .GetNextOccurrences(localTime.AddSeconds(-5), localTime.AddSeconds(5))
+            .Count();
+
+        var files = string.Join(", ", Directory.EnumerateFiles(GetTaskConfigDirectoryPath(context)));
+        log.LogInformation($"[{DateTime.UtcNow}][L:{localTime}] Timer triggered, [#{numOccurrences}] found config files: {files}");
     }
+
+    public static string GetTaskConfigDirectoryPath(ExecutionContext context)
+        => Path.Combine(
+            context.FunctionAppDirectory,
+            Constants.TaskConfigsDirectoryName);
 }
