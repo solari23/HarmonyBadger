d5012a8cf9f2b609d7b0ab2823f47a524fb93247
Alexandre Kerametlian <akerametlian@outlook.com>
2023-05-19 23:25:14 -0700

    Add authorize request to MS IdP

--- src/HarmonyBadger/IdentityAuthorization/AuthEndpointFunction.cs
+++ src/HarmonyBadger/IdentityAuthorization/AuthEndpointFunction.cs
@@ -1,28 +1,61 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 
 namespace HarmonyBadger.IdentityAuthorization;
 
-public static class AuthEndpointFunction
+public class AuthEndpointFunction
 {
     [FunctionName("HarmonyBadger_AuthEndpoint_Get")]
-    public static async Task<IActionResult> RunGetAsync(
+    public IActionResult RunGet(
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "authorization")] HttpRequest req,
         ILogger log)
     {
-        log.LogInformation("C# HTTP trigger function processed a request.");
-        return new OkObjectResult("Executed GET");
+        var authorizeQueryBuilder = new QueryStringBuilder();
+        authorizeQueryBuilder.AddParameter("client_id", "49ad3364-fa8b-4b1f-9761-5a6bc32cacf2");
+        authorizeQueryBuilder.AddParameter("response_type", "code+id_token", urlEncode: false);
+        authorizeQueryBuilder.AddParameter("redirect_uri", $"https://{req.Host}/authorization");
+        authorizeQueryBuilder.AddParameter("scope", "openid+email+offline_access+mail.send", urlEncode: false);
+        authorizeQueryBuilder.AddParameter("response_mode", "form_post");
+        authorizeQueryBuilder.AddParameter("prompt", "login");
+        authorizeQueryBuilder.AddParameter("nonce", Guid.NewGuid().ToString());
+
+        var authorizeUriBuilder = new UriBuilder("https://login.microsoftonline.com/common/oauth2/v2.0/authorize");
+        authorizeUriBuilder.Query = authorizeQueryBuilder.Build();
+
+        return new RedirectResult(authorizeUriBuilder.Uri.ToString());
     }
 
     [FunctionName("HarmonyBadger_AuthEndpoint_Post")]
-    public static async Task<IActionResult> RunPostAsync(
+    public async Task<IActionResult> RunPostAsync(
         [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "authorization")] HttpRequest req,
         ILogger log)
     {
         log.LogInformation("C# HTTP trigger function processed a request.");
-        return new OkObjectResult("Executed POST");
+
+        using var formReader = new FormReader(req.Body);
+        var requestForm = await formReader.ReadFormAsync();
+
+        if (requestForm.ContainsKey("error"))
+        {
+            var error = requestForm["error"].First();
+            var errorDescription = requestForm.ContainsKey("error_description")
+                ? requestForm["error_description"].First()
+                : string.Empty;
+            var errorString = $"IdP reported an error during authorization.\nError: {error}\nDescription: {errorDescription}";
+
+            // Return the error as a 200 so it'll render in browser.
+            // The HTTP status doesn't actually matter here.
+            log.LogError(errorString);
+            return new OkObjectResult(errorString);
+        }
+
+        var idToken = requestForm["id_token"].First();
+        var authCode = requestForm["code"].First();
+
+        return new OkObjectResult("Refresh Token Saved.");
     }
 }
---
+++ src/HarmonyBadger/QueryStringBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using System.Text.Encodings.Web;
+
+namespace HarmonyBadger;
+
+public class QueryStringBuilder
+{
+    private readonly StringBuilder queryBuilder = new StringBuilder();
+
+    public void AddParameter(string name, string value, bool urlEncode = true)
+    {
+        if (queryBuilder.Length > 0)
+        {
+            queryBuilder.Append('&');
+        }
+
+        queryBuilder.Append(name);
+        queryBuilder.Append('=');
+        queryBuilder.Append(urlEncode ? UrlEncoder.Default.Encode(value) : value);
+    }
+
+    public string Build() => queryBuilder.ToString();
+}
